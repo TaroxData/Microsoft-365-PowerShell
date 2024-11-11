f76651e30763758b120735051d29ed91b97d4153
TaroxAG <156064014+TaroxAG@users.noreply.github.com>
2024-11-11 10:18:36 +0100

    Update Block-OutlookNew-Upgrade.ps1

--- Block-OutlookNew-Upgrade.ps1
+++ Block-OutlookNew-Upgrade.ps1
@@ -19,22 +19,18 @@ if (Test-Path $registryPath) {
     if ($null -ne $value) {
         # Check if the value is set to 0
         if ($value.$valueName -eq 0) {
-            Write-Output "Registry key and DWORD exist, and NewOutlookMigrationUserSetting is set to 0."
             exit 0
         }
         else {
-            Write-Output "Registry key and DWORD exist, but NewOutlookMigrationUserSetting is not set to 0."
             setRegistryKey
         }
     }
     else {
         # DWORD does not exist
-        Write-Output "Registry key exists, but the DWORD NewOutlookMigrationUserSetting does not exist."
         setRegistryKey
     }
 }
 else {
     # Registry path does not exist
-    Write-Output "Registry path does not exist."
     setRegistryKey
 }
